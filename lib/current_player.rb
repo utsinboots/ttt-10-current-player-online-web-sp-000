98e27bf01fcb8b01ec5166c166b0333b75dbf14b
Utshant Gurung <utshantgurung@gmail.com>
2021-05-01 07:24:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,11 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  
-  if counter % 2 == 0
-    return X
-  else if counter % 2 == 1
-    O
+  if turn_count(board) % 2 == 0
+    "X"
+  else if turn_count(board) % 2 == 1
+    "O"
   end
 end
-=end
