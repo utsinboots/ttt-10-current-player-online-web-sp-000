0aa01d001d1ce7496eddd639ad948ecad866f9c9
Utshant Gurung <utshantgurung@gmail.com>
2021-05-01 07:06:16 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,8 @@
-counter = 0
 def turn_count(board)
+  counter = 0
   board.each do |index_value|
     if index_value == "X" || index_value == "O"
       counter += 1
-    else
-      turn_count(board)
     end
   end
 end
